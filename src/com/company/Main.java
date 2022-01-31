290cdf30140913f8c2880b04f409466aa0c5376f
kungen <andy.alavinasab@gmail.com>
2022-01-31 19:28:31 +0100

    Commit Andy Alavinasab

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("jävla skitkod");
         System.out.println("Emel pushing p, Yoorkevich pushing p");
+        System.out.println("Jag älskar Göteborg");
+        System.out.println("Jag gillar inte Borås");
         System.out.println("Jag älskar Snuseborg");
+        System.out.println("Ima off myself");
     }
 }

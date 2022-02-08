83b2a3e7ed109f7884d7d931cbf8e56baf265fe6
Max Yoorkevich <maxy@student.chalmers.se>
2022-02-08 15:59:59 +0100

    lägg till movement för Player

--- src/com/company/model/Player.java
+++ src/com/company/model/Player.java
@@ -2,14 +2,48 @@ package com.company.model;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 public class Player {
     private Point2D.Double position;
+    private final int size = 1;
 
     public Point2D.Double getPosition() {
         return position;
     }
     public void setPosition(double x, double y) {
         this.position = new Point2D.Double(x, y);
+    }
+
+    void drawPlayer(Graphics2D g){
+//        g.setColor(Color.BLUE);
+//        g.fillRect(getPosition().x, getPosition().y, size, size);
+        Rectangle2D player = new Rectangle2D.Double(getPosition().x, getPosition().y, size, size);
+        Graphics2D g2 = (Graphics2D) g;
+    }
+
+    private void moveEast(){
+        setPosition(getPosition().x + 1, getPosition().y);
+    }
+    private void moveWest(){
+        setPosition(getPosition().x - 1, getPosition().y);
+    }
+    private void moveNorth(){
+        setPosition(getPosition().x, getPosition().y + 1);
+    }
+    private void moveSouth(){
+        setPosition(getPosition().x , getPosition().y - 1);
+    }
+    private void moveNorthEast(){
+        setPosition(getPosition().x + 1, getPosition().y + 1);
+    }
+    private void moveNorthWest(){
+        setPosition(getPosition().x - 1, getPosition().y + 1);
+    }
+    private void moveSouthEast(){
+        setPosition(getPosition().x - 1, getPosition().y + 1);
+    }
+    private void moveSouthWest(){
+        setPosition(getPosition().x - 1, getPosition().y - 1);
     }
 }

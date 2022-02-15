acbf927a7a3251ea4c83b89ab76de0a5b4cd0e66
emme388 <emilberzelius@gmail.com>
2022-02-15 15:20:05 +0100

    commit - Collission class

--- src/com/company/controller/CollisionCheck.java
+++ src/com/company/controller/CollisionCheck.java
@@ -4,7 +4,7 @@ import com.company.view.PrippsView;
 
 public class CollisionCheck {
     PrippsView view;
-
+    int a = 0;
     public CollisionCheck(PrippsView view){
         this.view = view;
     }
